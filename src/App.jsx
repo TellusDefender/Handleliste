945334276b581e40f18c343c39b7fcf39637c67f
TellusDefender <stephen.pereira.sp@gmail.com>
2026-02-10 19:13:18 +0100

    1

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Addform from "AddForm";
+import ShoppingList from "ShoppingList";
+import './App.css'
+
+
+
+function App() {
+
+}  
+
+
+console.log()
+
+useEffect(() => {
+
+
+  const totalQuantity = cart.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
+}
+
+    setCartQuantity(totalQuantity);
+
+    const total = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0)
+   setTotalSum(total)
+  }, [cart]);
+
+
+function Page(){
+    return(
+          <main>
+            <CategoryTitle />
+            <Products products={products} setCart={setCart} />
+          </main>
+    )
+  }
+
+
+export default App
